7ada8b5ddc04c13e352e32a747212dbc35e83978
sandiPC <sandi@tafio.com>
2025-03-12 10:59:25 +0700

    update

--- app/Jobs/DeleteOrders.php
+++ app/Jobs/DeleteOrders.php
@@ -2,12 +2,14 @@
 
 namespace App\Jobs;
 
+use App\Models\Produksi;
 use Illuminate\Support\Facades\DB;
 
 class DeleteOrders
 {
     public function deleteOrders()
     {
+        $batal_id = Produksi::ambilFlow('batal');
         // Get marketplace contact IDs
         $marketplaceContactIds = DB::table('kontaks')
             ->where('marketplace', 1)
@@ -16,13 +18,13 @@ class DeleteOrders
         // Delete orders and their details in a single query per table
         DB::table('orders')
             ->join('order_details', 'orders.id', '=', 'order_details.order_id')
-            ->where('order_details.produksi_id', 9)
+            ->where('order_details.produksi_id', $batal_id)
             ->whereIn('orders.kontak_id', $marketplaceContactIds)
             ->delete();
 
         DB::table('order_details')
             ->join('orders', 'orders.id', '=', 'order_details.order_id')
-            ->where('order_details.produksi_id', 9)
+            ->where('order_details.produksi_id', $batal_id)
             ->whereIn('orders.kontak_id', $marketplaceContactIds)
             ->delete();
     }
