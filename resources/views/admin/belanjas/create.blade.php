8312bcac9a5b08271f1f2ca2bdbd339061b4df07
sandiPC <sandi@tafio.com>
2025-03-15 10:56:12 +0700

    belanja

--- resources/views/admin/belanjas/create.blade.php
+++ resources/views/admin/belanjas/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title')
-    Create Orders
+    Create Belanjas
 @endsection
 
 @section('content')
@@ -187,8 +187,8 @@
 @endsection
 
 @push('after-scripts')
-    <script src="https://unpkg.com/@trevoreyre/autocomplete-js"></script>
-    <link rel="stylesheet" href="https://unpkg.com/@trevoreyre/autocomplete-js/dist/style.css" />
+<script src="{{ asset('js/autocomplete.min.js') }}"></script>
+<link rel="stylesheet" href="{{ asset('js/autocomplete.css') }}">
     <script>
         new Autocomplete('#autocomplete', {
             search: input => {
